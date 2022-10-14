f071b1749ec5aeda3ab7206b380471e90a0bc39a
ٴ <104566003+ivwy@users.noreply.github.com>
2022-10-14 13:36:42 +0900

    loop false

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -4,31 +4,26 @@ textWrapper.innerHTML = textWrapper.textContent.replace(
     /([^\x00-\x80]|\w)/g,"<span class='letter'>$&</span>"
 );
 
-anime
-    .timeline({
-        loop: false
-    })
-    .add({
-        targets: ".intro-title .letter",
-        translateX: [140, 0],
-        translateZ: 0,
-        opacity: [0, 1],
-        easing: "easeOutExpo",
-        duration: 1400,
-        delay: function(el, i) {
-            return 500 + 50 * i;
-        }
-    })
-    .add({
-        targets: ".intro-title .letter",
-        translateX: [0, -140],
-        opacity: [1, 0],
-        easing: "easeInExpo",
-        duration: 1200,
-        delay: function(el, i) {
-            return 700 + 50 * i;
-        }
-    });
+anime.timeline().add({
+    targets: ".intro-title .letter",
+    translateX: [140, 0],
+    translateZ: 0,
+    opacity: [0, 1],
+    easing: "easeOutExpo",
+    duration: 1400,
+    delay: function(el, i) {
+        return 500 + 50 * i;
+    }
+}).add({
+    targets: ".intro-title .letter",
+    translateX: [0, -140],
+    opacity: [1, 0],
+    easing: "easeInExpo",
+    duration: 1200,
+    delay: function(el, i) {
+        return 700 + 50 * i;
+    }
+});
 
 TweenMax.to(".loader", 2.2, {
     delay: 2,
